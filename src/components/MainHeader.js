15ae6538083f11ee43abff6a8e2ca9c1cdc7dd3b
Ibraheem <101558762+IbraheemNaveed@users.noreply.github.com>
2023-01-05 14:38:39 +0500

    color

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -5,9 +5,9 @@ const MainHeader = () => {
    <>
    <main>
    <head>
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Berkshire+Swash&display=swap" rel="stylesheet"/>
+   <link rel="preconnect" href="https://fonts.googleapis.com"/>
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+   <link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@700&display=swap" rel="stylesheet"/>
 </head>
     <div class="relative px-6 lg:px-8 bg-opacity-60 ">
       <div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
@@ -20,7 +20,7 @@ const MainHeader = () => {
             </div> */}
           </div>
           <div>
-            <h1 class="text-4xl font-display tracking-tight text-[#852727] sm:text-center  sm:text-6xl" >If IT Consulting is an ART <br/> <br/> We are the MICHAELANGELOS <br/> of this sphere</h1>
+            <h1 class="text-4xl font-display tracking-tight text-[#852727] text-center  sm:text-6xl" >If IT Consulting is an ART  <br/><a className="sm:text-3xl text-xl " > We are the MICHAELANGELOS  of this sphere</a></h1>
             {/* <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p> */}
             {/* <div class="mt-8 flex gap-x-4 sm:justify-center">
               <a href="#" class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      display: [ "Berkshire Swash", "cursive"],
+      display: [ "Rowdies", "cursive"],
     },
     extend: {},
   },
